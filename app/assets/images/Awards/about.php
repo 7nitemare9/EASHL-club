5d5c84828ba3c52e0134174e54a4a0183b8ea864
7nitemare9 <7nitemare9@gmail.com>
2014-03-22 18:12:58 +0100

    fix2

---
+++ app/assets/images/Awards/about.php
@@ -0,0 +1,36 @@
+﻿
+
+<div class="box">
+					
+				
+					<div class="content">
+						<!-- News Subject -->
+<h3><img src="images/about.jpg" alt="" width="675" height="64" /></h3>
+<p>Kristinehamn Bombers är en EASHL klubb skapad 2012 som fokuserar sig på realistisk och respektabel hockey. Målet är att sammla vuxna och duktiga personer och forma en gemenskap som har roligt ihop och respekterar varandra. Målet är att vara ett slagkraftig och sammansvetsat lag för NHL 12, och delta i Europeen Hockey League där de bästa lagen i Europa deltar. </p>
+<p>Som spelare i Bombers har du några grundregler att följa för allas trevnad, om de inte följs hör du inte hemma i vårat lag.</p>
+<p><strong><u>REGLEMENTE</u></strong></p>
+<div align="left">
+  <p><strong>§ </strong><em>Du kommer få kritik då och då, det gäller ta kritiken på rätt sätt, och även ge kritik respektfullt.</em></p>
+</div>
+<div align="left">
+  <p><strong>§ </strong><em>Visa respekt för varandra, gå inte bakom ryggen på varandra eller snacka skit om dina lagkamrater. </em></p>
+</div>
+<div align="left">
+  <p><strong>§</strong> <em>Inget rage quitting, ha tålamod och kom ihåg, det är bara ett videospel, så chilla!</em></p>
+</div>
+<div align="left">
+  <p><strong>§</strong> <em>Var osjälvisk, den roligaste hockeyn är när alla är delaktiga i spelet, vi behöver inga OTP hjältar.</em></p>
+</div>
+<div align="left">
+  <p><strong>§</strong> <em>Fuska inte, Inge Statsboosting eller Glitch, spela smart så går det att göra mål iaf.</em></p>
+</div>
+<div align="left">
+  <p><strong>§</strong> <em>Följ våran <a href="./index.php?id=5">spelidé</a>, det är inte så svårt, spela för laget och inte för &quot;jaget&quot;.</em></p>
+</div>
+<p>&nbsp;</p>
+<strong><img src="./images/BombersLogo.png" <img src="./images/jerset.png alt="Matchtröjja Säsong 12/13" /></strong>
+<p>MVH General Manager Mattias Nilsson aka Dexrion</p>
+<p>&nbsp;</p>
+
+                  </div>	
+				</div>
