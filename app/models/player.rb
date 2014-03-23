d439f6bf6dc184bf30dcfa0f23329c87ba755d74
7nitemare9 <7nitemare9@gmail.com>
2014-03-23 13:04:48 +0100

    test navbarpics

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,7 +4,9 @@ require 'nokogiri'
 require 'open-uri'
 
 	def self.getPage(page, team)
-		url = "http://www.easportsworld.com/en_US/clubs/partial/NHL14XBX/" + team + "/" +page
+		# old easportsworld url
+		# url = "http://www.easportsworld.com/en_US/clubs/partial/NHL14XBX/" + team + "/" +page 
+		url = "http://www.easports.com/iframe/nhl14proclubs/api/platforms/xbox/clubs/" + team + "/" +page
 		begin
 			doc = Nokogiri::HTML(open(url))
 		rescue => e
@@ -16,24 +18,26 @@ require 'open-uri'
 	def self.playerData(doc)
 		member = {}
 		list = []
-		doc.css("tr").each do |player|
-			unless player.at_css(".align-left div").blank? or player.at_css(".strong:nth-child(4)").blank? then
-				member = {:name => player.at_css(".align-left div").text,
-									:goals => player.at_css(".strong:nth-child(4)").text,
-									:assists => player.at_css(".strong:nth-child(5)").text,
-									:points => player.at_css(".strong:nth-child(6)").text,
-									:hits => player.at_css(".strong:nth-child(11)").text }
-				unless find_by_name(member[:name]) then
-					create!(member)
-					member[:status] = 'created'
-				else
-					db_player = find_by_name(member[:name])
-					db_player.update_attributes(member)
-					member[:status] = 'updated'					
-				end
-				list.push(member)
-			end		
-		end
+		#old easportsworld way to get data
+		#doc.css("tr").each do |player|
+		#	unless player.at_css(".align-left div").blank? or player.at_css(".strong:nth-child(4)").blank? then
+		#		member = {:name => player.at_css(".align-left div").text,
+		#							:goals => player.at_css(".strong:nth-child(4)").text,
+		#							:assists => player.at_css(".strong:nth-child(5)").text,
+		#							:points => player.at_css(".strong:nth-child(6)").text,
+		#							:hits => player.at_css(".strong:nth-child(11)").text }
+		#		unless find_by_name(member[:name]) then
+		#			create!(member)
+		#			member[:status] = 'created'
+		#		else
+		#			db_player = find_by_name(member[:name])
+		#			db_player.update_attributes(member)
+		#			member[:status] = 'updated'					
+		#		end
+		#		list.push(member)
+		#	end		
+		#end
+		
 		return list		
 	end
 
