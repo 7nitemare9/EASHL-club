f3e69333e20508861a6514414d5e43fa7293be89
root <root@ip-10-227-206-185.eu-west-1.compute.internal>
2014-05-09 21:11:31 +0000

    "Removed php files"

--- public/bhf/cache/data_cfg_template_ca_black.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1430657164
-175
-a:5:{s:4:"name";s:8:"CA Black";s:9:"copyright";s:28:"&copy; 2007-2011 ST Software";s:7:"version";s:6:"1.0.11";s:17:"template_bitfield";s:4:"lNg=";s:8:"filetime";i:1398619479;}
