b41b4f745200b199b42fa6fbbb8c8e1bb6a108b3
7nitemare9 <7nitemare9@gmail.com>
2014-05-10 12:27:08 +0200

    no edit, delete links unless admin in posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,15 @@ class ApplicationController < ActionController::Base
   include PhpbbAuth
   before_filter :set_current_user
 
+  class NoUser < Struct.new(:user_name)
+  end
+
   def set_current_user
     @current_user = current_user
+    unless @current_user
+      @current_user = NoUser.new
+      @current_user.user_name = "not logged in"
+    end
+    @current_user    
   end
 end
