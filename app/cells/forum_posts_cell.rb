733fe71cf38be2a3c3ea55d5ff309853ec4d404a
7nitemare9 <7nitemare9@gmail.com>
2014-07-10 18:48:22 +0200

    added logged in check to forum posts cell

--- app/cells/forum_posts_cell.rb
+++ app/cells/forum_posts_cell.rb
@@ -4,4 +4,11 @@ class ForumPostsCell < Cell::Rails
     @posts = ForumPost.topics
     render
   end
+
+  def not_logged_in
+    item = { link: "#", title: "Not signed in", dc_creator: "" }
+    temp = { items: [OpenStruct.new(item)] }
+    @posts = OpenStruct.new temp
+    render view: :recent
+  end
 end
