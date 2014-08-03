b6dfebd0292ea509ba9cba66763fd09fa7e7455b
7nitemare9 <7nitemare9@gmail.com>
2014-08-03 17:17:16 +0200

    shouts as json for cljs shoutbox

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,21 +1,29 @@
 # Shoutbox controller
 class ShoutsController < ApplicationController
   def index
-    @shouts = Shout.find_all
-    render layout: false
+    respond_to do |format|
+      format.html {render :html => Shout.find_all}
+      format.json {render :json => Shout.find_all}
+    end
   end
 
   def create
     Shout.create(shout_params)
-    render :shout_save
+    #render :shout_save
   end
 
   def show
+    respond_to do |format|
+      format.json {render json: {user: @current_user.user_name}}
+    end
   end
 
   def destroy
     @post = Shout.find(params[:id])
     redirect_to root_path if @post.destroy
+  end
+
+  def user
   end
 
   private
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,6 +3,6 @@ class Shout < ActiveRecord::Base
   validates :name, :message, presence: true
 
   def self.find_all
-    find(:all, limit: 5, order: 'id DESC')
+    find(:all, order: 'id DESC')
   end
 end
