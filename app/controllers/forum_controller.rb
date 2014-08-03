b3ae9fdb48e895617483d38dc14786731c59d1e6
7nitemare9 <7nitemare9@gmail.com>
2014-08-03 18:06:35 +0200

    forum links go to iframe

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -2,4 +2,8 @@
 class ForumController < ApplicationController
   def index
   end
+
+  def show
+    @forum = "http://bombers-hockey.com/bhf/" + params[:id] + "." + params[:format] + "?f=" + params[:f] + "&t=" + params[:t] + "&p=" + params[:p]
+  end
 end
