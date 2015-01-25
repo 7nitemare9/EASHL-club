56f8ad981ab65e4d04d6357aed3bd83e0a24de07
7nitemare9 <7nitemare9@gmail.com>
2015-01-25 11:40:29 +0100

    Removed unnecesary phpbb directory

--- public/bhf_off/cache/data_cfg_theme_ca_black.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1430657164
-165
-a:5:{s:4:"name";s:8:"CA Black";s:9:"copyright";s:28:"&copy; 2007-2011 ST Software";s:7:"version";s:6:"1.0.11";s:14:"parse_css_file";b:1;s:8:"filetime";i:1398619486;}
