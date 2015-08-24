27252eabdbafc821923e521c3dff7d0046f61f64
7nitemare9 <7nitemare9@gmail.com>
2015-08-24 20:56:21 +0200

    bug fix in lineup

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,5 +1,10 @@
 # Authhelper
 module AuthHelper
   def is_admin
+    if @current_user
+      redirect_to root_path unless @current_user.user_name == 'admin'
+    else
+      redirect_to root_path
+    end
   end
 end
