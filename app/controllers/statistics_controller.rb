c96fb4e8bd3b23641560e6dff3d3eb729bcf4bc7
7nitemare9 <7nitemare9@gmail.com>
2015-09-20 18:26:58 +0200

    some tweaks of statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,7 @@
 class StatisticsController < ApplicationController
   def index
     respond_to do |format|
-      format.html {render :html => @stats = Statistic.all_stats}
+      format.html {render :html => "nothing"}
       format.json {render :json => Statistic.stats}
     end
   end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -87,14 +87,16 @@ class Statistic
     unique_player_names(goalies).map do |name|
       goalie = {name: name, goals_against: 0, saves: 0, GAA: 0,
         shots_against: 0, SO: 0, save_percent: 0, games: 0}
-      goalies.each do |player|
-        if name == player.first.personaName
-          goalie[:goals_against] += player.first.glga.to_i
-          goalie[:saves] += player.first.glsaves.to_i
-          goalie[:shots_against] += player.first.glshots.to_i
-          p player.first.glshots.to_i
-          goalie[:SO] += 1 if (player.first.glga == '0')
-          goalie[:games] += 1
+      goalies.each do |players|
+        players.each do |player|
+          if name == player.personaName
+            goalie[:goals_against] += player.glga.to_i
+            goalie[:saves] += player.glsaves.to_i
+            goalie[:shots_against] += player.glshots.to_i
+            p player.glshots.to_i
+            goalie[:SO] += 1 if (player.glga == '0')
+            goalie[:games] += 1
+          end
         end
       end
       goalie[:save_percent] = (goalie[:saves] / goalie[:shots_against].to_f) * 100
@@ -109,9 +111,11 @@ class Statistic
       forward = {name: name, assists: 0, giveaways: 0, goals: 0,
                  hits: 0, pims: 0, plus_minus:  0, points: 0, shots: 0, takeaways: 0,
         shot_percent: 0, games: 0}
-      forwrds.each do |player|
-        if name == player.first.personaName
-          forward = player_stats_adder(forward, player.first)
+      forwrds.each do |players|
+        players.each do |player|
+          if name == player.personaName
+            forward = player_stats_adder(forward, player)
+          end
         end
       end
       forward[:shot_percent] = (forward[:goals] / forward[:shots].to_f) * 100
